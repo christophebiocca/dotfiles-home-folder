13e30fdf5f5a996f3ec3ebff512d83e54e9d8f28
Christophe Biocca <christophe.biocca@gmail.com>
2014-05-26 18:50:03 -0400

    Updates xmonad config.

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,4 +1,14 @@
 import XMonad
 import XMonad.Config.Xfce
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 
-main = xmonad xfceConfig { modMask = mod4Mask }
+main = xmonad xfceConfig
+    { modMask = mod4Mask
+    , manageHook = manageDocks
+        <+> composeAll
+            [ isFullscreen --> doFullFloat
+            , className =? "Xfce4-notifyd" --> doIgnore
+            ]
+        <+> manageHook defaultConfig
+    }
